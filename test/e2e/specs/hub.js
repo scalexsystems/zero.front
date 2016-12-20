32857b97a5b1ec6d809b8ad75b4d82a6386698da
Rahul Kadyan <hi@znck.me>
2016-12-20 15:13:15 +0530

    :hankey: Drop em

--- test/e2e/specs/hub.js
+++ test/e2e/specs/hub.js
@@ -10,14 +10,6 @@ module.exports = {
       .assert.elementPresent('.hub-container', 'hub is reachable')
       .assert.elementPresent('.hub-sidebar-left > .group-list', 'group list is visible')
       .assert.elementPresent('.group-list-container .group-list-item:nth-child(1)', 'has at least one group')
-
-      .click('.group-list-container .group-list-item:nth-child(1)').pause(1000)
-      .assert.elementPresent('.layout-box', 'open first group')
-      .assert.containsText('.hub-content .message-box-title', 'A Private', 'visiting correct group')
-
-      // Click on `Private` link
-      .click('.hub-sidebar-left > .btn-group:nth-child(1) a:nth-child(1)')
-      .assert.elementPresent('.hub-sidebar-left > .user-list', 'user list is visible')
       .end();
   },
 };
